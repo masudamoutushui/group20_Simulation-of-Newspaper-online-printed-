8f6bac5fbeab082e49996c8dd57b1107f217f0f1
cis101 <ccc@gmail.com>
2025-07-27 12:27:09 +0600

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.oop.group20.group20_simulationofnewspaperonlineprinted {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.oop.group20.group20_simulationofnewspaperonlineprinted to javafx.fxml;
+    exports com.oop.group20.group20_simulationofnewspaperonlineprinted;
+}
