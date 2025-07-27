390c51aa31246f1a48d834ca6f228f63786bca3f
cis101 <ccc@gmail.com>
2025-07-27 12:53:13 +0600

    create a separated package for admin user

---
+++ src/main/java/com/oop/group20/group20_simulationofnewspaperonlineprinted/masuda/AdvertisementDashboardController.java
@@ -0,0 +1,7 @@
+package com.oop.group20.group20_simulationofnewspaperonlineprinted.masuda;
+
+public class AdvertisementDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
