32d4a0ebf2794a26e7281a052aab3794b51222ee
muaaz121 <muaazkhan7278364@gmail.com>
2025-08-01 23:39:12 +0600

    just did basic structure and scene switching task between goals

--- src/main/java/com/oop/group20/group20_simulationofnewspaperonlineprinted/Muaaz/EditorInChiefController.java
+++
@@ -1,7 +0,0 @@
-package com.oop.group20.group20_simulationofnewspaperonlineprinted.Muaaz;
-
-public class EditorInChiefController
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
---
+++ src/main/java/com/oop/group20/group20_simulationofnewspaperonlineprinted/Muaaz/EditorInChiefGoal7.java
@@ -0,0 +1,14 @@
+package com.oop.group20.group20_simulationofnewspaperonlineprinted.Muaaz;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class EditorInChiefGoal7 {
+    @FXML private VBox underReviewColumn;
+    @FXML private VBox approvedColumn;
+    @FXML private VBox scheduledColumn;
+    @FXML private VBox publishedColumn;
+
+    // TODO: Load articles grouped by status into respective columns
+    // TODO: Implement drag and drop or status change logic with validation
+}
