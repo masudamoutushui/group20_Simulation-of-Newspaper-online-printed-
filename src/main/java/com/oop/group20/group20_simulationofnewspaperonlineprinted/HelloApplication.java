75a1d1bcb766a2b6146a45580757ffa90337df2e
muaaz121 <muaazkhan7278364@gmail.com>
2025-08-16 21:38:37 +0600

    added more goal

--- src/main/java/com/oop/group20/group20_simulationofnewspaperonlineprinted/HelloApplication.java
+++ src/main/java/com/oop/group20/group20_simulationofnewspaperonlineprinted/HelloApplication.java
@@ -10,7 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Log in.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
---
+++ src/main/java/com/oop/group20/group20_simulationofnewspaperonlineprinted/Muaaz/TrialController.java
@@ -0,0 +1,7 @@
+package com.oop.group20.group20_simulationofnewspaperonlineprinted.Muaaz;
+
+public class TrialController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
